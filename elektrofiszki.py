b93cc1476670ffdc46b798e3c131e075736e000a
Lukasz Koszyk <vocativus@awk5.pl>
2022-06-02 00:08:23 +0200

    Commit

--- elektrofiszki.py
+++ elektrofiszki.py
@@ -6,32 +6,91 @@ from PyQt5.QtGui import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtWidgets import *
 from gtts.lang import tts_langs
-import sys, threading, time, numpy as np, datetime, os, traceback, warnings, json, glob, random
+import sys, threading, time, numpy as np, datetime, os, traceback, warnings, ujson as json, glob, random, hashlib, argparse, colorama
 from gtts import gTTS
 from collections import deque
 
 lvl = {
-  '0' : 0, # 0
-  '1' : 300, # 5min
-  '2' : 600, # 10min
-  '3' : 900, # 15min
-  '4' : 12*3600, # 12h
-  '5' : 24*3600, # 24h
-  '6' : 2*24*3600, # 2d
-  '7' : 3*24*3600, # 3d
-  '8' : 5*24*3600, # 5d
-  '9' : 8*24*3600, # 8d
-  '10' : 10*24*3600, # 10d
-  '11' : 14*24*3600, # 14d
-  '12': 21*24*3600, # 21d
-  '13' : 28*24*3600, # 28d
-  '14': 35*24*3600, # 35d
-  '15': 42*24*3600, # 42d
-  '16': 47*24*3600, # 47d
-  '17': 54*24*3600, # 54d
-  '18': 61*24*3600, # 61d
-  '19': 68*24*3600, # 68d
-  '20': 72*24*3600, # 72d
+  '0': {
+    'incr_t': 0,
+    'incr': '1',
+    'decr': '0',
+  },
+  '1': {
+    'incr_t': 300, #5min
+    'incr': '2',
+    'decr': '0',
+  },
+  '2': {
+    'incr_t': 900, #15min
+    'incr': '3',
+    'decr': '0',
+  },
+  '3': {
+    'incr_t': 1*24*3600, # 1d
+    'incr': '4',
+    'decr': '0',
+  },
+  '4': {
+    'incr_t': 2*24*3600, #2d
+    'incr': '5',
+    'decr': '2',
+  },
+  '5': {
+    'incr_t': 3*24*3600, #3d
+    'incr': '6',
+    'decr': '2',
+  },
+  '6': {
+    'incr_t': 7*24*3600, #7d
+    'incr': '7',
+    'decr': '3',
+  },
+  '7': {
+    'incr_t': 7*24*3600, #7d
+    'incr': '8',
+    'decr': '3',
+  },
+  '8': {
+    'incr_t': 14*24*3600, #14d
+    'incr': '9',
+    'decr': '4',
+  },
+  '9': {
+    'incr_t': 14*24*3600, #14d
+    'incr': '10',
+    'decr': '4',
+  },
+  '10': {
+    'incr_t': 30*24*3600, #30d
+    'incr': '11',
+    'decr': '5',
+  },
+  '11': {
+    'incr_t': 30*24*3600, #30d
+    'incr': '12',
+    'decr': '5',
+  },
+  '12': {
+    'incr_t': 60*24*3600, #60d
+    'incr': '13',
+    'decr': '5',
+  },
+  '13': {
+    'incr_t': 120*24*3600, #120d
+    'incr': '14',
+    'decr': '5',
+  },
+  '14': {
+    'incr_t': 240*24*3600, #240d
+    'incr': '15',
+    'decr': '5',
+  },
+  '15': {
+    'incr_t': 300*24*3600, #300d
+    'incr': '15',
+    'decr': '5',
+  },
 }
 
 q_speech = deque(maxlen=10)
@@ -49,14 +108,22 @@ def f_txt_to_speech():
     if kill_thread:
       break
 
-    item = q_speech.pop()
+    item = q_speech.popleft()
 
-    tts = gTTS('\n'.join([s + '.' for s in item['txt']]), lang=item['lang'], slow=False)
-    tts.save('/tmp/a.mp3')
+    filename = str(hashlib.sha384(str(item['txt']).encode()).hexdigest())
+    filename_full_path = 'elektrofiszki_audio/' + filename + '.mp3'
+
+    if not os.path.isdir('elektrofiszki_audio'):
+      os.mkdir('elektrofiszki_audio')
+
+    if not os.path.isfile(filename_full_path):
+      tts = gTTS('\n'.join([s + '.' for s in item['txt']]), lang=item['lang'], slow=False)
+      tts.save(filename_full_path)
+
     if item['slow']:
-      os.system('mpv --speed=0.8 --really-quiet /tmp/a.mp3')
+      os.system('mpv --speed=0.8 --really-quiet ' + filename_full_path)
     else:
-      os.system('mpv --really-quiet /tmp/a.mp3')
+      os.system('mpv --really-quiet ' + filename_full_path)
 
 
 #############################################################################################
@@ -330,8 +397,17 @@ class Window(QWidget):
     self.main_menu5.clicked.connect(self.f_main_menu)
 
     self.question_words = QPlainTextEdit()
+    q_font = self.question_words.font()
+    q_font.setPointSize(16)
+    self.question_words.setFont(q_font)
+
+
+
     self.answer_words = QLineEdit()
     self.answer_words.returnPressed.connect(self.f_check_answer)
+    a_font = self.question_words.font()
+    a_font.setPointSize(16)
+    self.answer_words.setFont(a_font)
 
     self.question_label = QLabel("NULL")
     self.question_label.setFont(self.headline)
@@ -343,6 +419,9 @@ class Window(QWidget):
 
     self.read_question = QPushButton('Read', self)
     self.read_question.clicked.connect(self.f_read_question)
+
+    self.add_this_response = QPushButton('Add this response', self)
+    self.add_this_response.clicked.connect(self.f_add_this_response)
 
     self.win5_msg = QLabel("-")
     self.win5_msg.setFont(self.headline)
@@ -360,6 +439,7 @@ class Window(QWidget):
     layout3.addWidget(self.answer_label)
     layout3.addWidget(self.answer_words)
     layout3.addWidget(self.check_answer)
+    layout3.addWidget(self.add_this_response)
     layout3.addStretch()
 
     layout4 = QHBoxLayout()
@@ -384,6 +464,45 @@ class Window(QWidget):
     self.win.addWidget(self.win5)
 
 #############################################################################################
+
+  def f_add_this_response(self):
+    choosen_dict = self.dicts_list_box.currentText()
+    choosen_direction = self.dicts_direction_box.currentText()
+    f = open(choosen_dict, 'r')
+    data = json.loads(f.read())
+    f.close()
+    l1 = data['lang1_long']
+    l2 = data['lang2_long']
+    if choosen_direction == l1 + ' TO ' + l2:
+      q_lang_verb = 'lang1'
+      q_lang_verb_answer = 'lang2'
+      q_lang_timestamp_verb = 'l1_to_l2_timestamp'
+      q_lang_level_verb = 'l1_to_l2_level'
+    else:
+      q_lang_verb = 'lang2'
+      q_lang_verb_answer = 'lang1'
+      q_lang_timestamp_verb = 'l2_to_l1_timestamp'
+      q_lang_level_verb = 'l2_to_l1_level'
+
+    question = self.question_words.toPlainText().split('\n')
+    index = None
+
+    for i in range(len(data['data'])):
+      if data['data'][i][q_lang_verb] == question:
+        index = i
+        break
+
+    if index == None:
+      print("Internal error - can't find asked question in json")
+      return
+
+    data['data'][index][q_lang_verb_answer].append(self.answer_words.text())
+
+    f = open(choosen_dict, 'w', encoding='utf8')
+    f.write(json.dumps(data, indent=4, ensure_ascii=False))
+    f.close()
+
+    self.f_check_answer()
 
 
   def f_check_answer(self):
@@ -420,22 +539,18 @@ class Window(QWidget):
       return
 
     if self.answer_words.text() in data['data'][index][q_lang_verb_answer]:
+      self.f_read_answer(slow=False)
       self.win5_msg.setText('-')
-      data['data'][index][q_lang_level_verb] += 1
+      data['data'][index][q_lang_level_verb] = lvl[str(data['data'][index][q_lang_level_verb])]['incr']
       ok = True
     else:
       self.win5_msg.setText('Wrong answer. Write one of correct answer: ' + ';'.join(data['data'][index][q_lang_verb_answer]))
-      self.f_read_question()
-      data['data'][index][q_lang_level_verb] -= 1
+      self.f_read_question(slow=False)
+      self.f_read_answer(slow=False)
+      data['data'][index][q_lang_level_verb] = lvl[str(data['data'][index][q_lang_level_verb])]['decr']
       ok = False
 
-    if data['data'][index][q_lang_level_verb] > 20:
-      data['data'][index][q_lang_level_verb] = 20
-
-    if data['data'][index][q_lang_level_verb] < 0:
-      data['data'][index][q_lang_level_verb] = 0
-
-    data['data'][index][q_lang_timestamp_verb] = int(time.time()) + int(lvl[str(data['data'][index][q_lang_level_verb])])
+    data['data'][index][q_lang_timestamp_verb] = int(time.time()) + int(lvl[str(data['data'][index][q_lang_level_verb])]['incr_t'])
 
     f = open(choosen_dict, 'w', encoding='utf8')
     f.write(json.dumps(data, indent=4, ensure_ascii=False))
@@ -444,6 +559,44 @@ class Window(QWidget):
     if ok:
       self.f_ask_a_question()
 
+
+  def f_read_answer(self, slow=True):
+    global q_speech
+
+    choosen_dict = self.dicts_list_box.currentText()
+    choosen_direction = self.dicts_direction_box.currentText()
+
+    f = open(choosen_dict, 'r')
+    data = json.loads(f.read())
+    f.close()
+
+    l1 = data['lang1_long']
+    l2 = data['lang2_long']
+    if choosen_direction == l1 + ' TO ' + l2:
+      q_lang_verb = 'lang1'
+      a_lang_verb = 'lang2'
+    else:
+      q_lang_verb = 'lang2'
+      a_lang_verb = 'lang1'
+
+    question = self.question_words.toPlainText().split('\n')
+    index = None
+
+    for i in range(len(data['data'])):
+      if data['data'][i][q_lang_verb] == question:
+        index = i
+        break
+
+    if index == None:
+      print("Internal error - can't find asked question in json")
+      return
+
+    item = {
+      'txt': data['data'][index][a_lang_verb],
+      'lang': data[a_lang_verb],
+      'slow': slow,
+    }
+    q_speech.append(item)
 
   def f_read_question(self, slow=True):
     global q_speech
@@ -509,7 +662,7 @@ class Window(QWidget):
     self.f_read_question(slow=False)
     self.answer_words.clear()
     self.answer_words.setFocus()
-    self.win5_msg.setText('Write one of answer which exists in dict and hit enter or check')
+    self.win5_msg.setText( '(' + str(len(available_questions)) + ') Write one of answer which exists in dict and hit enter or check')
 
 
   def f_main_menu(self):
@@ -660,11 +813,40 @@ class Window(QWidget):
     self.dict_item_no.setMinimum(-1)
     self.dict_item_no.setMaximum(-1)
 
-  def f_save_new_word(self):
-    choosen_dict = self.dicts_list_box.currentText()
-    choosen_direction = self.dicts_direction_box.currentText()
-    tab_lang1 = self.lang1_words.toPlainText().split('\n')
-    tab_lang2 = self.lang2_words.toPlainText().split('\n')
+  def print_err(self, msg):
+    print(colorama.Fore.RED + str(msg) + colorama.Style.RESET_ALL)
+
+  def f_save_new_word(self, cli_mode = False, a_choosen_dict = None, a_lang1_short = None, a_lang2_short = None, a_lang1_words = None, a_lang2_words = None):
+
+    if cli_mode:
+      if a_choosen_dict == None:
+        self.print_err('var a_choosen_dict is None')
+        sys.exit(1)
+      if a_lang1_short == None:
+        self.print_err('var a_lang1_short is None')
+        sys.exit(1)
+      if a_lang2_short == None:
+        self.print_err('var a_lang2_short is None')
+        sys.exit(1)
+      if a_lang1_words == None:
+        self.print_err('var a_lang1_words is None')
+        sys.exit(1)
+      if a_lang2_words == None:
+        self.print_err('var a_lang2_words is None')
+        sys.exit(1)
+
+      choosen_dict = a_choosen_dict
+      tab_lang1 = a_lang1_words
+      tab_lang2 = a_lang2_words
+
+      if not os.path.isfile(choosen_dict):
+        self.print_err(choosen_dict + ' does not exist!')
+        sys.exit(1)
+    else:
+      choosen_dict = self.dicts_list_box.currentText()
+      tab_lang1 = self.lang1_words.toPlainText().split('\n')
+      tab_lang2 = self.lang2_words.toPlainText().split('\n')
+
     for i in range(len(tab_lang1)):
       if tab_lang1[i] == '':
         del tab_lang1[i]
@@ -674,7 +856,7 @@ class Window(QWidget):
         del tab_lang2[i]
 
     if len(tab_lang1) == 0 or len(tab_lang2) == 0:
-      print('Missing words in at least one of text spaces')
+      self.print_err('Missing words in at least one of text spaces')
       self.win4_output_msg.setText('Missing words in at least one of text spaces')
       return
 
@@ -682,23 +864,39 @@ class Window(QWidget):
     data = json.loads(f.read())
     f.close()
 
+    if cli_mode:
+      if a_lang1_short != data['lang1']:
+        self.print_err('lang1 name in dict is different than set in command line')
+        sys.exit(1)
+      if a_lang2_short != data['lang2']:
+        self.print_err('lang2 name in dict is different than set in command line')
+        sys.exit(1)
+
     for i in tab_lang1:
       for j in data['data']:
         if i in j['lang1']:
-          self.win4_output_msg.setText('STOP! ' + i + ' already exists in dict')
-          return
+          if cli_mode:
+            self.print_err('STOP! "' + i + '" already exists in dict')
+            sys.exit(1)
+          else:
+           self.win4_output_msg.setText('\033[91m STOP! "' + i + '" already exists in dict')
+           return
 
     for i in tab_lang2:
       for j in data['data']:
         if i in j['lang2']:
-          self.win4_output_msg.setText('STOP! ' + i + ' already exists in dict')
-          return
+          if cli_mode:
+            self.print_err('STOP! "' + i + '" already exists in dict')
+            sys.exit(1)
+          else:
+           self.win4_output_msg.setText('STOP! ' + i + ' already exists in dict')
+           return
 
     item = {
       'lang1': tab_lang1,
       'lang2': tab_lang2,
-      'l1_to_l2_level': 0,
-      'l2_to_l1_level': 0,
+      'l1_to_l2_level': "0",
+      'l2_to_l1_level': "0",
       'l1_to_l2_timestamp': 0,
       'l2_to_l1_timestamp': 0,
     }
@@ -708,11 +906,13 @@ class Window(QWidget):
     f.write(json.dumps(data, indent=4, ensure_ascii=False))
     f.close()
 
-    self.dict_item_no.setMinimum(-1)
-    self.dict_item_no.setMaximum(len(data['data'])-1)
-    self.dict_item_no.setValue(len(data['data'])-1)
-    self.win4_output_msg.setText('Saved. Choose next option')
-
+    if cli_mode:
+      print('OK')
+    else:
+      self.dict_item_no.setMinimum(-1)
+      self.dict_item_no.setMaximum(len(data['data'])-1)
+      self.dict_item_no.setValue(len(data['data'])-1)
+      self.win4_output_msg.setText('Saved. Choose next option')
 
 
 
@@ -795,23 +995,56 @@ class Window(QWidget):
     f.close()
     self.label_create.setText("File created. Go to main menu and open.")
 
+
 #############################################################################################
+parser = argparse.ArgumentParser(description='Elektrofiszki CLI')
+parser.add_argument('--cli_mode', action=argparse.BooleanOptionalAction,            help='turn on CLI mode')
+parser.add_argument('--dict_file',  dest='dict_file', required=False, type=str,     help='name of json dict file')
+parser.add_argument('--lang1_short',  dest='lang1_short', required=False, type=str, help='short lang1 name eg. pl, no')
+parser.add_argument('--lang2_short',  dest='lang2_short', required=False, type=str, help='short lang2 name eg. pl, no')
+parser.add_argument('--lang1_words',  dest='lang1_words', required=False, type=str, help='colon separated words for lang1')
+parser.add_argument('--lang2_words',  dest='lang2_words', required=False, type=str, help='colon separated words for lang2')
+args = parser.parse_args()
 
-
+#############################################################################################
 
 
 app = QApplication(sys.argv)
 screen = Window()
 
-thread_list = []
+if not args.cli_mode:
 
-t = threading.Thread(target=f_txt_to_speech)
-thread_list.append(t)
+  thread_list = []
 
-for thread in thread_list:
-    thread.start()
+  t = threading.Thread(target=f_txt_to_speech)
+  thread_list.append(t)
 
-screen.showMaximized()
-app.exec_()
+  for thread in thread_list:
+      thread.start()
 
-kill_thread = True
+  screen.showMaximized()
+  app.exec_()
+
+  kill_thread = True
+
+else:
+  if not args.dict_file:
+    screen.print_err('--dict_file required')
+    sys.exit(1)
+  if not args.lang1_short:
+    screen.print_err('--lang1_short required')
+    sys.exit(1)
+  if not args.lang2_short:
+    screen.print_err('--lang2_short required')
+    sys.exit(1)
+  if not args.lang1_words:
+    screen.print_err('--lang1_words required')
+    sys.exit(1)
+  if not args.lang2_words:
+    screen.print_err('--lang2_words required')
+    sys.exit(1)
+
+  lang1_words_arr = args.lang1_words.split(';')
+  lang2_words_arr = args.lang2_words.split(';')
+
+  screen.f_save_new_word(cli_mode=True, a_choosen_dict = args.dict_file, a_lang1_short = args.lang1_short, a_lang2_short = args.lang2_short, a_lang1_words = lang1_words_arr, a_lang2_words = lang2_words_arr)
